ce594fa0977431b675a8110c1b821245a071cf20
Shivanshu Mishra <shivanshu.mishra97@gmail.com>
2019-05-09 22:25:54 +0200

    Unbounded Wild Cards example

--- src/test/java/generics/GenericsTest.java
+++ src/test/java/generics/GenericsTest.java
@@ -22,9 +22,33 @@ public class GenericsTest {
 		objects.add("Hindi");// As string is subtype of objects
 		
 		Object language="Hindi";
-		//strings.add(language); Not possible as List<String> is not subtype of List<Object>
+		//strings.add(language); Not possible as List<String> is not subtype of List<Object>		
+	}
+	
+	@Test
+	public void testUnboundedWildCards(){
+		// A wild card is a type argument that uses a question mark
 		
+		//Limitation of unbounded Type arguments is that you can read from it but cannot write into it.
+		List<?> stuff=new ArrayList<>();
+		//stuff.add("This"); Not possible , only method which does not expected any type or read operation
+		Assert.assertEquals(0,stuff.size());
+		}
+	
+	@Test
+	public void testUnboundedWildCardUse(){
+		List<String> strings=new ArrayList<>();
+		List<Integer> ints=new ArrayList<>();
 		
+		strings.add("Hello");strings.add("Namaste");strings.add("ciao");
+		ints.add(1);ints.add(69);ints.add(23);
+		
+		printElement(strings);
+		printElement(ints);
+	}
+	
+	private void printElement(List<?> list){
+		list.forEach(System.out::println);		
 	}
 
 }
