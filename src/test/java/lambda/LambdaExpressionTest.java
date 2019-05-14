30efc8004f92fbfdbf7d15191f8e24dbc3bda6a3
Shivanshu Mishra <shivanshu.mishra97@gmail.com>
2019-05-14 22:31:58 +0200

    Constructor refernce examples

--- src/test/java/lambda/LambdaExpressionTest.java
+++ src/test/java/lambda/LambdaExpressionTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -83,8 +84,45 @@ public class LambdaExpressionTest {
 	@Test
 	// Class::instanceMethod
 	public void testClassInstanceMethodReference() {
+		// System.out.println("Method reference example by using class reference
+		// on instantiate method");
 		List<String> strings = Arrays.asList("this", "is", "a", "list", "of", "strings");
 		List<String> sorted = strings.stream().sorted(String::compareTo).collect(Collectors.toList());
-		sorted.forEach(System.out::println);
+		// sorted.forEach(System.out::println);
+
+		// Each method reference has equivalent lambda but not vice versa
+		// Lambda equivalent for above method reference
+		// System.out.println("Lambda equivalent of Method reference");
+		List<String> sortedLambda = strings.stream().sorted((s1, s2) -> s1.compareTo(s2)).collect(Collectors.toList());
+		// sortedLambda.forEach(System.out::println);
+		Assert.assertTrue("Sorted listed generated using lambda equivalent and method reference differs", sorted.equals(sortedLambda));
+	}
+	
+	@Test
+	public void testConstructorReference(){
+		List<String> names =
+			    Arrays.asList("Grace Hopper", "Barbara Liskov", "Ada Lovelace",
+			        "Karen Spärck Jones");
+		
+		List<Person> peoples=names.stream()
+				.map(Person::new)
+				.collect(Collectors.toList());
+		
+		List<Person> people_s=names.stream()
+				.map(name -> new Person(name))
+				.collect(Collectors.toList());
+		Assert.assertTrue("Number of names not equals to number of person", names.size()==peoples.size());
+		Assert.assertTrue("Number of names not equals to number of person",names.size()==people_s.size());
+		Assert.assertTrue("List of people generated from lambda and constructor refernce are not same", peoples.contains(people_s));
+	}
+	
+	private class Person{
+		private String name;
+		
+		public Person(){}
+		
+		public Person(String name){
+			this.name=name;
+		}
 	}
 }
