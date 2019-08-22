bbba59f9b1307bffd0af084ab69169ea8100672c
Shivanshu mishra <shivanshu.mishra97@gmail.com>
2019-08-22 21:56:54 +0200

    Few more Java.time recipies

--- src/test/java/time/DateTimeTest.java
+++ src/test/java/time/DateTimeTest.java
@@ -8,9 +8,13 @@ import java.time.LocalTime;
 import java.time.Month;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
 
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 /**
  * All Classes in date-time are immutable and Thread safe.
@@ -70,4 +74,45 @@ public class DateTimeTest {
 		System.out.println("Adding two months: " + Month.JANUARY.plus(2));
 		System.out.println("Subtracting a month: " + Month.MARCH.minus(1));
 	}
+	
+	@Test
+	public void testCreatingDateFromExisting() {
+		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate start=LocalDate.of(1988, Month.JULY, 30);
+		LocalDate afterOneDay=start.plusDays(1);
+		Assert.assertEquals("1988-07-31", formatter.format(afterOneDay));
+		
+		LocalDate afterOneMonth=start.plusMonths(1);
+		Assert.assertEquals("1988-08-30", formatter.format(afterOneMonth));
+		
+		LocalDate afterOneYear=start.plusYears(1);
+		Assert.assertEquals("1989-07-30", formatter.format(afterOneYear));
+		
+		LocalDate leapStart=LocalDate.of(2020, Month.FEBRUARY, 28);
+		LocalDate afterOneDayInLeapFeb=leapStart.plusDays(1);
+		Assert.assertEquals("2020-02-29", formatter.format(afterOneDayInLeapFeb));
+		
+		LocalDate after2Day=start.plus(2, ChronoUnit.DAYS);
+		Assert.assertEquals("1988-08-01",formatter.format(after2Day));
+		
+		LocalDate after3Decades=start.plus(3, ChronoUnit.DECADES);
+		Assert.assertEquals("2018-07-30",formatter.format(after3Decades));
+	}
+	
+	@Test
+	public void testCreatingTimeFromExisting() {
+		DateTimeFormatter formatter=DateTimeFormatter.ISO_LOCAL_TIME;
+		LocalTime startTime=LocalTime.of(21, 30,0,0);
+		LocalTime after1NanoSecond=startTime.plusNanos(1_000_000);
+		LocalTime after1Second= startTime.plusSeconds(1);
+		LocalTime after1Minues = startTime.plusMinutes(1);
+		LocalTime after1Hour = startTime.plusHours(1);
+		LocalTime after10Hours = startTime.plus(10, ChronoUnit.HOURS);
+		
+		Assert.assertEquals("21:30:00.001", formatter.format(after1NanoSecond));
+		Assert.assertEquals("21:30:01", formatter.format(after1Second));
+		Assert.assertEquals("21:31:00", formatter.format(after1Minues));
+		Assert.assertEquals("22:30:00", formatter.format(after1Hour));
+		Assert.assertEquals("07:30:00", formatter.format(after10Hours));		
+	}	
 }
